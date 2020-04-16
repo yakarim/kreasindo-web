ebcee0dec9a623483196df3838d7899a487aca7d
attar4443 <attar4443@gmail.com>
2020-04-16 08:06:26 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/yakarim/kreasindo-web
+
+go 1.13
+
+require (
+	github.com/savsgio/atreugo/v10 v10.4.2
+	github.com/savsgio/go-logger v1.0.0
+)
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"os"
+	"runtime"
+
+	"github.com/savsgio/atreugo/v10"
+	"github.com/savsgio/go-logger"
+)
+
+func main() {
+
+	port := os.Getenv("PORT")
+	var config atreugo.Config
+	if port == "8080" {
+		config = atreugo.Config{
+			Addr:     "0.0.0.0:8080",
+			Name:     "Kreasindo Pratama",
+			Compress: true,
+			LogLevel: logger.DEBUG,
+		}
+	} else {
+		runtime.GOMAXPROCS(runtime.NumCPU())
+		config = atreugo.Config{
+			Addr:              "0.0.0.0:" + port,
+			Name:              "Kreasindo Pratama",
+			ReduceMemoryUsage: true,
+			Compress:          true,
+			Concurrency:       3,
+			GracefulShutdown:  true,
+		}
+	}
+
+	g := atreugo.New(config)
+
+	if err := g.ListenAndServe(); err != nil {
+		panic(err)
+	}
+}
