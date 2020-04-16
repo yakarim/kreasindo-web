64d2dc700f7e007434b7e712ecf1e2672f6436bb
attar4443 <attar4443@gmail.com>
2020-04-16 08:46:41 +0700

    first commit

--- router.go
+++ router.go
@@ -3,5 +3,11 @@ package main
 import "github.com/savsgio/atreugo/v10"
 
 func router(ctx *atreugo.Atreugo) {
+	ctx.GET("/", Index)
 
+}
+
+// Index ...
+func Index(ctx *atreugo.RequestCtx) error {
+	return ctx.JSONResponse("halo walid", 200)
 }
