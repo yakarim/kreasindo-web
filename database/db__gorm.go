77e58d507eb9c7b02779ad4b60e9cf5a1af238ed
attar4443 <attar4443@gmail.com>
2020-04-21 09:01:17 +0700

    database commit

--- database/db__gorm.go
+++ database/db__gorm.go
@@ -13,38 +13,33 @@ import (
 // DB ... gorm
 var DB *gorm.DB
 
-const (
-	host     = "ec2-52-71-231-180.compute-1.amazonaws.com"
-	port     = 5432
-	database = "d4hq86psqtvd0e"
-	user     = "kwpicwgoeksjds"
-	pass     = "1ff0cd6a150c7486649d7c8ef2f7f12a5671567c8631374e8c742b75aabbcecc"
-)
-
 func init() {
-
+	const (
+		host     = "ec2-52-71-231-180.compute-1.amazonaws.com"
+		port     = 5432
+		database = "d4hq86psqtvd0e"
+		user     = "kwpicwgoeksjds"
+		pass     = "1ff0cd6a150c7486649d7c8ef2f7f12a5671567c8631374e8c742b75aabbcecc"
+		sslmode  = "require"
+	)
 	var db *gorm.DB
 	p := os.Getenv("PORT")
 	if p == "8080" {
-		psql := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-			"localhost", 5432, "postgres", "1234", "kreasindo")
-
-		b, err := gorm.Open("postgres", psql)
-		if err != nil {
-			log.Println(err)
-		}
-		db = b
+		db = pqsl("localhost", "postgres", "1234", "kreasindo", "disable", 5432)
 	} else {
-		psql := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=require",
-			host, port, user, pass, database)
-
-		b, err := gorm.Open("postgres", psql)
-		if err != nil {
-			log.Println(err)
-		}
-		db = b
+		db = pqsl(host, user, pass, database, sslmode, port)
 	}
 	db.AutoMigrate(&User{})
 	DB = db
 
+}
+
+func pqsl(host, user, pass, database, ssl string, port int) *gorm.DB {
+	psql := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s ", host, port, user, pass, database, ssl)
+
+	b, err := gorm.Open("postgres", psql)
+	if err != nil {
+		log.Println(err)
+	}
+	return b
 }
