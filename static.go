e7326702aafa09675ca059649803579c6af1e944
attar4443 <attar4443@gmail.com>
2020-05-11 08:20:48 +0700

    static

--- static.go
+++ static.go
@@ -18,7 +18,7 @@ func costumStatic(ctx *atreugo.Atreugo, name string) {
 	rootFS := &atreugo.StaticFS{
 		Root:            "./static/" + name,
 		AcceptByteRange: true,
-		CacheDuration:   15 * time.Minute,
+		CacheDuration:   15 * time.Hour,
 		Compress:        true,
 	}
 	ctx.StaticCustom("/"+name, rootFS)
